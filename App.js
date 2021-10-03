4aea32768002bd2ccefd4fda39167133b95cf8c4
anittasunny <anittasunny39@gmail.com>
2021-10-03 22:37:11 +0530

    code formatted

--- App.js
+++ App.js
@@ -1,27 +1,26 @@
-import React, { useEffect, useState } from 'react';
-import Page from './screens/page'
-import AnimatedSplash from "react-native-animated-splash-screen";   
+import React, {useEffect, useState} from 'react';
+import Page from './screens/page';
+import AnimatedSplash from 'react-native-animated-splash-screen';
 
 const App = () => {
-  const [hasAnimationPlayedOnce, setHasAnimationPlayedOnce] = useState(false)
-  
-  useEffect(()=>{
-    setTimeout(() => {
-      setHasAnimationPlayedOnce(true)
-    }, 2000);
-  }),[]
+  const [hasAnimationPlayedOnce, setHasAnimationPlayedOnce] = useState(false);
 
+  useEffect(() => {
+    setTimeout(() => {
+      setHasAnimationPlayedOnce(true);
+    }, 2000);
+  }),
+    [];
 
   return (
     <AnimatedSplash
-    translucent={true}
-    isLoaded={hasAnimationPlayedOnce}
-    logoImage={require("./assets/logo.png")}
-    backgroundColor={"#262626"}
-    logoHeight={800}
-    logoWidth={400}
-  >
-    <Page />
+      translucent={true}
+      isLoaded={hasAnimationPlayedOnce}
+      logoImage={require('./assets/logo.png')}
+      backgroundColor={'#262626'}
+      logoHeight={800}
+      logoWidth={400}>
+      <Page />
     </AnimatedSplash>
   );
 };
